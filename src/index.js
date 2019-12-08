6d9cc360734c56fe622c5aa1c170597cf24d3d6f
Wang Peng <947034046@qq.com>
2019-12-08 18:04:04 +0800

    支持 es6，react，less

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { helloworld } from './helloworld';
+
+document.write(helloworld());
+
+let arr = [1,2,3]
+
+let [one, two] = arr
+
+let foo = () => console.log(one, two)
+
+foo()
---
+++ src/search.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './search.less';
+
+function Search() {
+  return <div className="search-text">Search Text</div>;
+}
+
+ReactDOM.render(
+  <Search />,
+  document.getElementById('root')
+);
