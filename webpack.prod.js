042a54f74f93c49a6507ee1a2baa4a4aa3a6e8e6
Wang Peng <947034046@qq.com>
2019-12-18 00:17:17 +0800

    在webpack中使用ESLint

--- webpack.prod.js
+++ webpack.prod.js
@@ -53,7 +53,10 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        use: 'babel-loader'
+        use: [
+          'babel-loader',
+          'eslint-loader'
+        ]
       },
       {
         test: /\.css$/,
